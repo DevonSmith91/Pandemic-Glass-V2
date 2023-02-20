7ac90bb385f3638e6101a4cc27ff664c94424956
Devon Smith <devon.smith@futurefoundry.co>
2023-02-20 16:56:11 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Pandemic-Glass-V2`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
